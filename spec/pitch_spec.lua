0078afb55e0d321b7c74ec79ad657f1f879fca3a
Andre Soares <andregsilv@gmail.com>
2022-04-11 09:05:35 -0300

    Pitch working

--- spec/pitch_spec.lua
+++ spec/pitch_spec.lua
@@ -4,16 +4,80 @@ require "pitch"
 describe("Basic test", function()
    pitch = Pitch:new()
 
-   describe("Initial values", function()
-      assert.are.equal(0, pitch.step)
+   describe("Assert initial values from constructor are being set", function()
+      assert.are.equal(1, pitch.step)
       assert.are.equal(0, pitch.alteration)
       assert.are.equal(4, pitch.octave)
    end)
 
-   describe("Values should be set normalized", function()
+end)
 
-      pitch.step = -2
+describe("Step", function()
+
+   describe("Normal values should work", function()
+      pitch = Pitch:new()
+
+      pitch.step = 1
+      assert.are.equal(1, pitch.step)
+
+      pitch.step = 2
+      assert.are.equal(2, pitch.step)
+
+      pitch.step = 3
+      assert.are.equal(3, pitch.step)
+
+      pitch.step = 4
+      assert.are.equal(4, pitch.step)
+
+      pitch.step = 5
       assert.are.equal(5, pitch.step)
 
+      pitch.step = 6
+      assert.are.equal(6, pitch.step)
+
+      pitch.step = 7
+      assert.are.equal(7, pitch.step)
+   end)
+
+   describe("Bigger values should be normalized", function()
+      pitch = Pitch:new()
+
+      pitch.step = 8
+      assert.are.equal(1, pitch.step)
+
+      pitch.step = 9
+      assert.are.equal(2, pitch.step)
+
+      pitch.step = 10
+      assert.are.equal(3, pitch.step)
+
+      pitch.step = 11
+      assert.are.equal(4, pitch.step)
+
+      pitch.step = 12
+      assert.are.equal(5, pitch.step)
+
+      pitch.step = 13
+      assert.are.equal(6, pitch.step)
+
+      pitch.step = 14
+      assert.are.equal(7, pitch.step)
+
+      pitch.step = 15
+      assert.are.equal(1, pitch.step)
+
+   end)
+
+   describe("Zero or negative values should do nothing", function()
+      pitch = Pitch:new()
+
+      for goodValue = 1, 6 do
+         for badValue = -1, -20, -1 do
+            pitch.step = goodValue
+            pitch.step = badValue
+            assert.are.equal(goodValue, pitch.step)
+         end
+      end
+      
    end)
 end)
--- src/pitch.lua
+++ src/pitch.lua
@@ -1,9 +1,16 @@
 Pitch = require "newclass"(function(self)
-   self.step = 0
+   self._step = 1
    self.alteration = 0
    self.octave = 4
 end)
 
-function Pitch:set_step(value)
-   self.step = math.floor(self.step % 7)
+function Pitch:get_step()
+   return self._step
 end
+function Pitch:set_step(value)
+   if value < 1 then
+      return
+   end
+   self._step = (value-1) % 7 + 1
+end
+
