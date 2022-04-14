9881636d7e1d1c9f4c04f202015867f8f38eaa85
Andre Soares <andregsilv@gmail.com>
2022-04-14 12:05:49 -0300

    basic inversion rules

--- spec/interval_spec.lua
+++ spec/interval_spec.lua
@@ -1,6 +1,19 @@
 require "busted.runner"()
 require "interval"
 
+describe("Constructor", function()
+   int = Interval:new()
+
+   assert.are.equal(Interval.UNISON, int.size)
+   assert.are.equal(1, int.size)
+
+   assert.are.equal(Interval.PERFECT, int.quality)
+   assert.are.equal(0, int.quality)
+
+   assert.are.equal(Interval.ASCENDING, int.direction)
+   assert.are.equal(1, int.direction)
+end)
+
 describe("Getters and setters", function()
    describe("Size", function()
       describe("Values within range", function()
@@ -14,54 +27,90 @@ describe("Getters and setters", function()
 end)
 
 describe("Normalization", function()
-   describe("Unison should stay unison", function()
-      int = Interval:new()
-      int.size = 1
-      newInt = int.normalize()
-      assert.are.equal(1, newInt.size)
+      describe("Unison should stay unison", function()
+         int = Interval:new()
+         int.size = 1
+         newInt = int.normalize()
+         assert.are.equal(1, newInt.size)
+         assert.are.equal(Interval.UNISON, newInt.size)
+      end)
+      describe("Should always be in the first octave", function()
+         for s = 2, 127, 7 do
+            int = Interval:new()
+            int.size = s
+            newInt = int.normalize()
+            assert.are.equal(2, newInt.size)
+            assert.are.equal(Interval.SECOND, newInt.size)
+         end
+         for s = 3, 127, 7 do
+            int = Interval:new()
+            int.size = s
+            newInt = int.normalize()
+            assert.are.equal(3, newInt.size)
+            assert.are.equal(Interval.THIRD, newInt.size)
+         end
+         for s = 4, 127, 7 do
+            int = Interval:new()
+            int.size = s
+            newInt = int.normalize()
+            assert.are.equal(4, newInt.size)
+            assert.are.equal(Interval.FOURTH, newInt.size)
+         end
+         for s = 5, 127, 7 do
+            int = Interval:new()
+            int.size = s
+            newInt = int.normalize()
+            assert.are.equal(5, newInt.size)
+            assert.are.equal(Interval.FIFTH, newInt.size)
+         end
+         for s = 6, 127, 7 do
+            int = Interval:new()
+            int.size = s
+            newInt = int.normalize()
+            assert.are.equal(6, newInt.size)
+            assert.are.equal(Interval.SIXTH, newInt.size)
+         end
+         for s = 7, 127, 7 do
+            int = Interval:new()
+            int.size = s
+            newInt = int.normalize()
+            assert.are.equal(7, newInt.size)
+            assert.are.equal(Interval.SEVENTH, newInt.size)
+         end
+         for s = 8, 127, 7 do
+            int = Interval:new()
+            int.size = s
+            newInt = int.normalize()
+            assert.are.equal(8, newInt.size)
+            assert.are.equal(Interval.OCTAVE, newInt.size)
+         end
+      end)
    end)
-   describe("Should always be in the first octave", function()
-      for s = 2, 127, 7 do
-         int = Interval:new()
-         int.size = s
-         newInt = int.normalize()
-         assert.are.equal(2, newInt.size)
-      end
-      for s = 3, 127, 7 do
-         int = Interval:new()
-         int.size = s
-         newInt = int.normalize()
-         assert.are.equal(3, newInt.size)
-      end
-      for s = 4, 127, 7 do
-         int = Interval:new()
-         int.size = s
-         newInt = int.normalize()
-         assert.are.equal(4, newInt.size)
-      end
-      for s = 5, 127, 7 do
-         int = Interval:new()
-         int.size = s
-         newInt = int.normalize()
-         assert.are.equal(5, newInt.size)
-      end
-      for s = 6, 127, 7 do
-         int = Interval:new()
-         int.size = s
-         newInt = int.normalize()
-         assert.are.equal(6, newInt.size)
-      end
-      for s = 7, 127, 7 do
-         int = Interval:new()
-         int.size = s
-         newInt = int.normalize()
-         assert.are.equal(7, newInt.size)
-      end
-      for s = 8, 127, 7 do
-         int = Interval:new()
-         int.size = s
-         newInt = int.normalize()
-         assert.are.equal(8, newInt.size)
+
+   describe("Inversion", function()
+      describe("two times should result in normalization", function()
+         for s = 1, 127 do
+            int = Interval:new()
+            int.size = s
+            doubleInverted = int.invert().invert()
+            normalized = int.normalize()
+            assert.are.equal(normalized.size, doubleInverted.size)
+            assert.are.equal(normalized.quality, doubleInverted.quality)
+            assert.are.equal(normalized.direction, doubleInverted.direction)
+         end
+      end)
+
+   describe("size and direction should never should remain the same", function()
+      for _, d in pairs({1, -1}) do
+         for s = 1, 127 do
+            int = Interval:new()
+            int.size = s
+            int.direction = d
+
+            inverted = int.invert()
+            assert.are_not.equal(int.size, inverted.size)
+            assert.are_not.equal(int.direction, inverted.direction)
+         end
       end
    end)
 end)
--- src/interval.lua
+++ src/interval.lua
@@ -1,23 +1,43 @@
 require "utils"
 
-
 Interval = require "newclass"(function(self)
    self.size = 1
    self.quality = 0
-   self.direction = 0
+   self.direction = 1
 end)
 
+Interval.UNISON = 1
+Interval.SECOND = 2
+Interval.THIRD = 3
+Interval.FOURTH = 4
+Interval.FIFTH = 5
+Interval.SIXTH = 6
+Interval.SEVENTH = 7
+Interval.OCTAVE = 8
+
+Interval.AUGMENTED = 0
+Interval.MAJOR = 1
+Interval.PERFECT = 0
+Interval.MINOR = -1
+Interval.DIMINISHED = 0
+
+Interval.ASCENDING = 1
+Interval.DESCENDING = -1
+
 function Interval:normalize()
-   newInterval = Interval:new()
+   int = Interval:new()
    if self.size == 1 then
-      newInterval.size = 1
+      int.size = 1
    else
-      newInterval.size = (self.size -2) % 7 +2
+      int.size = (self.size - 2) % 7 + 2
    end
-   return newInterval
+   return int
 end
 
--- function Interval:invert()
---    newInterval = Interval:new()
---    return newInterval
--- end
+function Interval:invert()
+   norm = self.normalize()
+   int = Interval:new()
+   int.size = 8 - norm.size + 1
+   int.direction = -self.direction
+   return int
+end
