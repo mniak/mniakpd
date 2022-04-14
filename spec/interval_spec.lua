55998c002fa7deb317e3ceeed190d751bde15569
Andre Soares <andregsilv@gmail.com>
2022-04-14 12:06:47 -0300

    format

--- spec/interval_spec.lua
+++ spec/interval_spec.lua
@@ -27,78 +27,78 @@ describe("Getters and setters", function()
 end)
 
 describe("Normalization", function()
-      describe("Unison should stay unison", function()
-         int = Interval:new()
-         int.size = 1
-         newInt = int.normalize()
-         assert.are.equal(1, newInt.size)
-         assert.are.equal(Interval.UNISON, newInt.size)
-      end)
-      describe("Should always be in the first octave", function()
-         for s = 2, 127, 7 do
-            int = Interval:new()
-            int.size = s
-            newInt = int.normalize()
-            assert.are.equal(2, newInt.size)
-            assert.are.equal(Interval.SECOND, newInt.size)
-         end
-         for s = 3, 127, 7 do
-            int = Interval:new()
-            int.size = s
-            newInt = int.normalize()
-            assert.are.equal(3, newInt.size)
-            assert.are.equal(Interval.THIRD, newInt.size)
-         end
-         for s = 4, 127, 7 do
-            int = Interval:new()
-            int.size = s
-            newInt = int.normalize()
-            assert.are.equal(4, newInt.size)
-            assert.are.equal(Interval.FOURTH, newInt.size)
-         end
-         for s = 5, 127, 7 do
-            int = Interval:new()
-            int.size = s
-            newInt = int.normalize()
-            assert.are.equal(5, newInt.size)
-            assert.are.equal(Interval.FIFTH, newInt.size)
-         end
-         for s = 6, 127, 7 do
-            int = Interval:new()
-            int.size = s
-            newInt = int.normalize()
-            assert.are.equal(6, newInt.size)
-            assert.are.equal(Interval.SIXTH, newInt.size)
-         end
-         for s = 7, 127, 7 do
-            int = Interval:new()
-            int.size = s
-            newInt = int.normalize()
-            assert.are.equal(7, newInt.size)
-            assert.are.equal(Interval.SEVENTH, newInt.size)
-         end
-         for s = 8, 127, 7 do
-            int = Interval:new()
-            int.size = s
-            newInt = int.normalize()
-            assert.are.equal(8, newInt.size)
-            assert.are.equal(Interval.OCTAVE, newInt.size)
-         end
-      end)
+   describe("Unison should stay unison", function()
+      int = Interval:new()
+      int.size = 1
+      newInt = int.normalize()
+      assert.are.equal(1, newInt.size)
+      assert.are.equal(Interval.UNISON, newInt.size)
    end)
+   describe("Should always be in the first octave", function()
+      for s = 2, 127, 7 do
+         int = Interval:new()
+         int.size = s
+         newInt = int.normalize()
+         assert.are.equal(2, newInt.size)
+         assert.are.equal(Interval.SECOND, newInt.size)
+      end
+      for s = 3, 127, 7 do
+         int = Interval:new()
+         int.size = s
+         newInt = int.normalize()
+         assert.are.equal(3, newInt.size)
+         assert.are.equal(Interval.THIRD, newInt.size)
+      end
+      for s = 4, 127, 7 do
+         int = Interval:new()
+         int.size = s
+         newInt = int.normalize()
+         assert.are.equal(4, newInt.size)
+         assert.are.equal(Interval.FOURTH, newInt.size)
+      end
+      for s = 5, 127, 7 do
+         int = Interval:new()
+         int.size = s
+         newInt = int.normalize()
+         assert.are.equal(5, newInt.size)
+         assert.are.equal(Interval.FIFTH, newInt.size)
+      end
+      for s = 6, 127, 7 do
+         int = Interval:new()
+         int.size = s
+         newInt = int.normalize()
+         assert.are.equal(6, newInt.size)
+         assert.are.equal(Interval.SIXTH, newInt.size)
+      end
+      for s = 7, 127, 7 do
+         int = Interval:new()
+         int.size = s
+         newInt = int.normalize()
+         assert.are.equal(7, newInt.size)
+         assert.are.equal(Interval.SEVENTH, newInt.size)
+      end
+      for s = 8, 127, 7 do
+         int = Interval:new()
+         int.size = s
+         newInt = int.normalize()
+         assert.are.equal(8, newInt.size)
+         assert.are.equal(Interval.OCTAVE, newInt.size)
+      end
+   end)
+end)
 
-   describe("Inversion", function()
-      describe("two times should result in normalization", function()
-         for s = 1, 127 do
-            int = Interval:new()
-            int.size = s
-            doubleInverted = int.invert().invert()
-            normalized = int.normalize()
-            assert.are.equal(normalized.size, doubleInverted.size)
-            assert.are.equal(normalized.quality, doubleInverted.quality)
-            assert.are.equal(normalized.direction, doubleInverted.direction)
-         end
-      end)
+describe("Inversion", function()
+   describe("two times should result in normalization", function()
+      for s = 1, 127 do
+         int = Interval:new()
+         int.size = s
+         doubleInverted = int.invert().invert()
+         normalized = int.normalize()
+         assert.are.equal(normalized.size, doubleInverted.size)
+         assert.are.equal(normalized.quality, doubleInverted.quality)
+         assert.are.equal(normalized.direction, doubleInverted.direction)
+      end
+   end)
 
    describe("size and direction should never should remain the same", function()
       for _, d in pairs({1, -1}) do
