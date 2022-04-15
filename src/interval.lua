aae6d84cf71838f83f7fd0e7acbd171df7b86745
Andre Soares <andregsilv@gmail.com>
2022-04-15 11:41:51 -0300

    interval use raw_size

--- src/interval.lua
+++ src/interval.lua
@@ -1,10 +1,18 @@
 require "utils"
 
 Interval = require "newclass"(function(self)
-   self.size = 1
+   self.raw_size = 0
    self.quality = 0
    self.direction = 1
 end)
+
+function Interval:get_size()
+   return self.raw_size + 1
+end
+
+function Interval:set_size(value)
+   self.raw_size = value - 1
+end
 
 Interval.SIZE_UNISON = 1
 Interval.SIZE_SECOND = 2
@@ -26,10 +34,10 @@ Interval.DIRECTION_DESCENDING = -1
 
 function Interval:normalize()
    int = Interval:new()
-   if self.size == 1 then
-      int.size = 1
+   if self.raw_size == 0 then
+      int.raw_size = 0
    else
-      int.size = (self.size - 2) % 7 + 2
+      int.raw_size = (self.raw_size - 1) % 7 + 1
    end
    return int
 end
@@ -37,7 +45,7 @@ end
 function Interval:invert()
    norm = self.normalize()
    int = Interval:new()
-   int.size = 8 - norm.size + 1
+   int.raw_size = 7 - norm.raw_size
    int.direction = -self.direction
    return int
 end
